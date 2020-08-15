7db0ca5f3ecee8334a7cc48d2fb1933c86212406
arnav_agrawal <aa687@cornell.edu>
2020-08-15 13:04:38 -0400

    Added source archive and build distribution thru 'dist' folder

--- setup.py
+++ setup.py
@@ -6,3 +6,25 @@ Created on Fri Aug 14 00:41:53 2020
 @author: arnav
 """
 
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="example-pkg-arnav", # Replace with your own username
+    version="0.0.1",
+    author="Arnav Agrawal",
+    author_email="aa687@cornell.edu",
+    description="A small example package",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/arnav-agrawal/packaging_tutorial",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
+)
